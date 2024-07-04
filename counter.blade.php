5ad5cd4a45ec293d7cc153a9f81d8b9484062267
MAmojica <78264218+MAmojica@users.noreply.github.com>
2024-07-04 08:07:11 +0800

    Create counter.blade.php

---
+++ counter.blade.php
@@ -0,0 +1,5 @@
+<div class="flex justify-center">
+<button class="p-2 bg-blue-500 text-white rounded shadow ml-2" wire:click="decrement">-</button>
+    <h1 class="text-xl">{{ $counter }}</h1> 
+    <button class="p-2 bg-blue-500 text-white rounded shadow mr-2" wire:click="increment">+</button>
+</div>
